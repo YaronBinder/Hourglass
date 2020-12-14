8f6ee8d5d882b0686330d0aceb5ecd14bd6654d9
Yaron Binder <75532643+YaronBinder@users.noreply.github.com>
2020-12-14 14:01:03 +0200

    Update Hourglass.cpp

--- Hourglass.cpp
+++ Hourglass.cpp
@@ -1,6 +1,7 @@
-#include <stdio.h>
-#include <windows.h>
 #include <time.h>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
 
 int main()
 {
@@ -8,31 +9,73 @@ int main()
 	float totalTime, displaySec;
 	time_t start_t, end_t;
 	double diff_t;
+	do {
+		system("cls");
+		// input to the user
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+		printf("Enter the time in seconds: ");
+		scanf_s("%d", &seconds);
+		printf("Enter the size of the sand clock: ");
+		scanf_s("%d", &size);
 
-	// input to the user
-	printf("Enter the time in seconds: ");
-	scanf_s("%d", &seconds);
-	printf("Enter the size of the sand clock: ");
-	scanf_s("%d", &size);
+		displaySec = totalTime = seconds * ((size / 2) - 1);
+		displaySec = seconds;
+		size += !(size % 2);
+		for (l = 0; l < (size + 1) / 2; l++) {
+			time(&start_t);
+			for (i = 0, k = size - 1; i < size; i++, k--, totalTime--) {
+				for (j = 0; j < size; j++) {
+					if (i == j || j == i - 2 && j<k
+						|| j == k - 2 && j<i || j == k
+						|| j == k + 2 && j>i || j == i + 2 && j>k) {
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 5);
+						printf("0 ");
+					}
+					else if (j == i - 1 && j<k
+						|| j == k - 1 && j<i
+						|| j == k + 1 && j>i
+						|| j == i + 1 && j>k) {
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
+						printf("O ");
+					}
+					else if ((j > i && j<k && i>l - 1)
+						|| (j<i && j>k && l > k)) {
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+						printf("* ");
+					}
+					else printf("  ");
+				}
+				printf("\n");
+			}
+			if (displaySec > 60) {
+				for (i = 0; i < size - 17; i++) printf(" ");
+				printf("Time left: %d minutes and %d seconds", (int)displaySec / 60, (int)displaySec % 60);
+			}
+			else {
+				for (i = 0; i < size - 12; i++) printf(" ");
+				printf("Time left: %1.1f seconds", displaySec);
+			}
+			displaySec -= seconds / ((float)size / 2);
 
-	displaySec = totalTime = seconds * ((size / 2) - 1);
-	displaySec = seconds;
-	size += !(size % 2);
-	for (l = 0; l < (size + 1) / 2; l++) {
-		time(&start_t);
-		for (i = 0, k = size - 1; i < size; i++, k--, totalTime--) {
+			time(&end_t);
+			diff_t = difftime(end_t, start_t);
+			Sleep((1000 - diff_t) * (seconds / (size / 2)));
+			system("cls");
+		}
+		// Time out display
+		for (i = 0, k = size - 1; i < size; i++, k--) {
 			for (j = 0; j < size; j++) {
 				if (i == j || j == i - 2 && j<k
 					|| j == k - 2 && j<i || j == k
 					|| j == k + 2 && j>i || j == i + 2 && j>k) {
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 5);
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
 					printf("0 ");
 				}
 				else if (j == i - 1 && j<k
 					|| j == k - 1 && j<i
 					|| j == k + 1 && j>i
 					|| j == i + 1 && j>k) {
-					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
 					printf("O ");
 				}
 				else if ((j > i && j<k && i>l - 1)
@@ -44,52 +87,20 @@ int main()
 			}
 			printf("\n");
 		}
-		for (i = 0; i < size - 12; i++) printf(" ");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
 
-		printf("Time left: %1.1f seconds", displaySec);
-		displaySec -= seconds / ((float)size / 2);
+		// printing spaces to align the TIMES OUT! text to center
+		for (i = 0; i < size - 5; i++) printf(" ");
 
-		time(&end_t);
-		diff_t = difftime(end_t, start_t);
-		Sleep((1000 * (seconds / (size / 2))) - diff_t);
+		printf("TIMES OUT!");
+		// beep sound
+		Beep(1000, 400);
+		Beep(750, 200);
+		Beep(1000, 400);
+		Beep(750, 200);
+		Beep(1000, 400);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 		system("cls");
-	}
-	// Time out display
-	for (i = 0, k = size - 1; i < size; i++, k--) {
-		for (j = 0; j < size; j++) {
-			if (i == j || j == i - 2 && j<k
-				|| j == k - 2 && j<i || j == k
-				|| j == k + 2 && j>i || j == i + 2 && j>k) {
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
-				printf("0 ");
-			}
-			else if (j == i - 1 && j<k
-				|| j == k - 1 && j<i
-				|| j == k + 1 && j>i
-				|| j == i + 1 && j>k) {
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
-				printf("O ");
-			}
-			else if ((j > i && j<k && i>l - 1)
-				|| (j<i && j>k && l > k)) {
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-				printf("* ");
-			}
-			else printf("  ");
-		}
-		printf("\n");
-	}
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
-
-	// printing spaces to align the TIMES OUT! text to center
-	for (i = 0; i < size - 5; i++) printf(" ");
-
-	printf("TIMES OUT!");
-	// beep sound
-	Beep(1000, 400);
-	Beep(750, 200);
-	Beep(1000, 400);
-	Beep(750, 200);
-	Beep(1000, 400);
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+		printf("Again? Y/N ");
+	} while (_getch() == 'Y' || _getch() == 'y');
 }
